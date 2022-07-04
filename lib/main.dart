7106b6ea9d47ee38bf8e3e4ad7941db6994a70f0
lamser69 <jonatansagala22@gmail.com>
2022-07-04 12:41:54 +0700

    debugbaner

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData.dark().copyWith(
           colorScheme: kColorScheme,
